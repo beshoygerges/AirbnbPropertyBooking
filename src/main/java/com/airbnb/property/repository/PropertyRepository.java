beb6396d2f0780700bd21cbb3ca6462717186c4e
beshoygerges <beshoygerges9292@gmail.com>
2024-05-08 17:08:08 +0300

    Add row-level lock on property

--- src/main/java/com/airbnb/property/repository/PropertyRepository.java
+++ src/main/java/com/airbnb/property/repository/PropertyRepository.java
@@ -1,7 +1,9 @@
 package com.airbnb.property.repository;
 
 import com.airbnb.property.model.Property;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,6 @@ import java.util.UUID;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Property> findByUuid(UUID uuid);
 }
